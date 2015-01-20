641a5348a616564cd2783a12102e52815a0c2ac1
Phase <ph4sesaber@gmail.com>
2015-01-20 02:29:37 -0800

    Trying to get jQuery working with Ace

---
+++ docs/EasyAsXYZ.java
@@ -0,0 +1,16 @@
+
+//This is a single line comment!
+/*
+This is a multi line comment!
+*/
+
+//Java's objects are represented as classes, which have the sam ename as the file they're in.
+public class EasyAsXYZ{
+	
+	//The main method is called when the program is run.
+	public static void main(String... args){
+		//This is how you print text
+		System.out.println("Hello World!");
+	}
+
+}
