81e2bd6737ba1d652f213f4484317360d2c5d0d5
Phase <ph4sesaber@gmail.com>
2015-01-22 00:26:18 -0800

    Updates

--- js/loadEditor.js
+++ js/loadEditor.js
@@ -1,14 +1,14 @@
 function loadEditor(language, fileName){
 	var editor = ace.edit("editor");
 	editor.setTheme("ace/theme/cobalt");
-	editor.setValue("jQuery not loading file!\nMake sure you're at http://learn.5th.xyz/");
+	editor.setValue("AJAX file request denied!\nMake sure you're at http://learn.5th.xyz/");
 	var fontSize = 18;
 	editor.setFontSize(fontSize);
 	$.get("http://learn.5th.xyz/docs/" + fileName , function(data){
 		editor.setValue(data);
 		editor.getSession().setMode("ace/mode/" + language);
 	});
-	var lineBreaks = (editor.getValue().match(/\n/gm)||[]).length;
+	var lineBreaks = (editor.getValue().match(/(\r\n|\n|\r)/gm)||[]).length;
 	document.getElementById("editor").style.height = ((fontSize * (lineBreaks + 1)) + 6) + "px";
 	editor.gotoLine(1);
 	//editor.setReadOnly(true);
