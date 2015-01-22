cd678222abcfd4a8ba1e90eb7367a47de4134689
Phase <ph4sesaber@gmail.com>
2015-01-22 00:11:06 -0800

    Updated Textbox height & width

--- js/loadEditor.js
+++ js/loadEditor.js
@@ -2,11 +2,14 @@ function loadEditor(language, fileName){
 	var editor = ace.edit("editor");
 	editor.setTheme("ace/theme/cobalt");
 	editor.setValue("jQuery not loading file!\nMake sure you're at http://learn.5th.xyz/");
+	var fontSize = 18;
+	editor.setFontSize(fontSize);
 	$.get("http://learn.5th.xyz/docs/" + fileName , function(data){
 		editor.setValue(data);
 		editor.getSession().setMode("ace/mode/" + language);
 	});
+	var lineBreaks = (editor.getValue().match(/\n/g)||[]).length;
+	document.getElementById("editor").style.height = ((fontSize * (lineBreaks + 1)) + 6) + "px";
 	editor.gotoLine(1);
-	editor.setReadOnly(true);
-        editor.setFontSize(24);
+	//editor.setReadOnly(true);
 }
